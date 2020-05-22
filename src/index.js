267d13abc475b53a4d797912197d503f08549279
[nyambayasgalan] <nyambayasgalan@gmail.com>
2020-05-22 10:46:30 -0700

    props

--- src/index.js
+++ src/index.js
@@ -6,7 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-         <CommentDetail />
+         <CommentDetail 
+         author="Sam" 
+         timeAgo="Today at 4:45PM" 
+         comment="Nice blog post!" 
+         avatar={faker.image.avatar()}
+         />
+         <CommentDetail 
+         author="Alex" 
+         timeAgo="Today at 2:00AM" 
+         comment="Very useful information. Thank you!" 
+         avatar={faker.image.avatar()}
+         />
+         <CommentDetail 
+         author="Jane" 
+         timeAgo="Yesterday at 5:00PM" 
+         comment="I like the subject!" 
+         avatar={faker.image.avatar()}
+         />
        </div>
     );
 };
